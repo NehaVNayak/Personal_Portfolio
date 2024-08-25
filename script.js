2ea3f14aa686be89608de559822dbd2b9c21947b
Neha Nayak <139000685+NehaVNayak@users.noreply.github.com>
2024-08-25 12:38:45 +0530

    Personal_Portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+    // Here you can add functionality to send the form data to your email or server
+});
